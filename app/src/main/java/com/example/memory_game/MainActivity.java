25510c71820536b5ce4bd9ebcd722fbb46bce1a7
PrachurGupta <prachur12@gmail.com>
2022-06-02 07:41:05 +0530

    final

--- app/src/main/java/com/example/memory_game/MainActivity.java
+++ app/src/main/java/com/example/memory_game/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.memory_game;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -201,8 +205,188 @@ public class MainActivity extends AppCompatActivity {
             iv.setImageResource(image206);
         }
 
+        //check which image is selected and it to temporary variable
         if(cardNumber == 1) {
+            firstCard = cardArray[card];
+            if (firstCard > 200){
+                firstCard = firstCard - 100;
+            }
+            cardNumber = 2;
+            clickedFirst = card;
 
+            iv.setEnabled(false);
+        } else if ((cardNumber == 2)) {
+            secondCard = cardArray[card];
+            if (secondCard > 200){
+                secondCard = secondCard - 100;
+            }
+            cardNumber = 1;
+            getClickedSecond = card;
+
+            iv_11.setEnabled(false);
+            iv_12.setEnabled(false);
+            iv_13.setEnabled(false);
+            iv_14.setEnabled(false);
+            iv_21.setEnabled(false);
+            iv_22.setEnabled(false);
+            iv_23.setEnabled(false);
+            iv_24.setEnabled(false);
+            iv_31.setEnabled(false);
+            iv_32.setEnabled(false);
+            iv_33.setEnabled(false);
+            iv_34.setEnabled(false);
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //check if the selected images are equal
+                    calculate();
+                }
+            }, 1000);
+        }
+    }
+
+    private void calculate() {
+        //if images are equal remove them and add point
+        if (firstCard == secondCard) {
+            if (clickedFirst == 0) {
+                iv_11.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 1) {
+                iv_12.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 2) {
+                iv_13.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 3) {
+                iv_14.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 4) {
+                iv_21.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 5) {
+                iv_22.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 6) {
+                iv_23.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 7) {
+                iv_24.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 8) {
+                iv_31.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 9) {
+                iv_32.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 10) {
+                iv_33.setVisibility(View.INVISIBLE);
+            } else if (clickedFirst == 11) {
+                iv_34.setVisibility(View.INVISIBLE);
+            }
+
+            if (getClickedSecond == 0) {
+                iv_11.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 1) {
+                iv_12.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 2) {
+                iv_13.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 3) {
+                iv_14.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 4) {
+                iv_21.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 5) {
+                iv_22.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 6) {
+                iv_23.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 7) {
+                iv_24.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 8) {
+                iv_31.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 9) {
+                iv_32.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 10) {
+                iv_33.setVisibility(View.INVISIBLE);
+            } else if (getClickedSecond == 11) {
+                iv_34.setVisibility(View.INVISIBLE);
+            }
+
+            //add players to the correct player
+            if (turn == 1){
+                playerPoints++;
+                tv_p1.setText("P1: " + playerPoints);
+            }else if (turn == 2){
+                cpuPoints++;
+                tv_p2.setText("P2: " + cpuPoints);
+            }
+        } else {
+            iv_11.setImageResource(R.drawable.quemark);
+            iv_12.setImageResource(R.drawable.quemark);
+            iv_13.setImageResource(R.drawable.quemark);
+            iv_14.setImageResource(R.drawable.quemark);
+            iv_21.setImageResource(R.drawable.quemark);
+            iv_22.setImageResource(R.drawable.quemark);
+            iv_23.setImageResource(R.drawable.quemark);
+            iv_24.setImageResource(R.drawable.quemark);
+            iv_31.setImageResource(R.drawable.quemark);
+            iv_32.setImageResource(R.drawable.quemark);
+            iv_33.setImageResource(R.drawable.quemark);
+            iv_34.setImageResource(R.drawable.quemark);
+
+
+            //change the player turn
+            if (turn == 1) {
+                turn = 2;
+                tv_p1.setTextColor(Color.GRAY);
+                tv_p2.setTextColor(Color.BLACK);
+            }else if (turn == 2) {
+                turn = 2;
+                tv_p2.setTextColor(Color.GRAY);
+                tv_p1.setTextColor(Color.BLACK);
+            }
+        }
+        iv_11.setEnabled(true);
+        iv_12.setEnabled(true);
+        iv_13.setEnabled(true);
+        iv_14.setEnabled(true);
+        iv_21.setEnabled(true);
+        iv_22.setEnabled(true);
+        iv_23.setEnabled(true);
+        iv_24.setEnabled(true);
+        iv_31.setEnabled(true);
+        iv_32.setEnabled(true);
+        iv_33.setEnabled(true);
+        iv_34.setEnabled(true);
+
+        //check if the game is over
+        checkEnd();
+    }
+
+    private void checkEnd(){
+        if (iv_11.getVisibility() == View.INVISIBLE &&
+                iv_12.getVisibility() == View.INVISIBLE &&
+                iv_13.getVisibility() == View.INVISIBLE &&
+                iv_14.getVisibility() == View.INVISIBLE &&
+                iv_21.getVisibility() == View.INVISIBLE &&
+                iv_22.getVisibility() == View.INVISIBLE &&
+                iv_23.getVisibility() == View.INVISIBLE &&
+                iv_24.getVisibility() == View.INVISIBLE &&
+                iv_31.getVisibility() == View.INVISIBLE &&
+                iv_32.getVisibility() == View.INVISIBLE &&
+                iv_33.getVisibility() == View.INVISIBLE &&
+                iv_34.getVisibility() == View.INVISIBLE ){
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+            alertDialogBuilder
+                    .setMessage("GAME OVER!!\nP1: " + playerPoints + "\nP2: " + cpuPoints)
+                    .setCancelable(false)
+                    .setPositiveButton("NEW", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finish();;
+                        }
+                    });
+            AlertDialog alertDialog= alertDialogBuilder.create();
+            alertDialog.show();
         }
     }
 
